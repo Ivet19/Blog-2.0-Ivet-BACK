3bacb0bf986787e8e67d8bce28ae53132e5c6025
Ivet Lopez <ivet19@live.com>
2025-04-29 11:46:45 +0200

    Change connection string of database

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import connectToDatabase from "./database/connectToDatabase.js";
 const port = process.env.PORT || 4000;
 
 await connectToDatabase(
-  "mongodb+srv://IsaSaete:IsaSaete@julian-cluster0.3u5nph9.mongodb.net/Blog",
+  "mongodb+srv://ivet19:Mondongo2025@mondongo.g5yiugk.mongodb.net/Blog",
 );
 
 startServer(Number(port));
